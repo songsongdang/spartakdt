a2b1560750356963ffb37ce86924393661d541cb
Eungyeong Song <jade92948@gmail.com>
2025-06-26 10:54:42 +0900

    장바구니에서 삭제한 물품 복구 기능 추가

--- bin/test_1.dart
+++ bin/test_1.dart
@@ -6,12 +6,26 @@ class Product {
   int price;
 
   Product(this.name, this.price);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Product &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          price == other.price;
+
+  @override
+  int get hashCode => name.hashCode ^ price.hashCode;
 }
 
 class ShoppingMall {
   List<Product> products;
   Map<Product, int> cart = {};
   int totalPrice = 0;
+
+  /// 여러 번 삭제한 상품들을 누적 저장
+  Map<Product, int> lastRemovedItems = {};
 
   ShoppingMall(this.products);
 
@@ -103,6 +117,10 @@ class ShoppingMall {
       return;
     }
 
+    // 누적 삭제 정보 저장
+    lastRemovedItems[selectedProduct] =
+        (lastRemovedItems[selectedProduct] ?? 0) + count;
+
     // 상품 개수 차감
     cart[selectedProduct] = currentCount - count;
     totalPrice -= selectedProduct.price * count;
@@ -110,6 +128,19 @@ class ShoppingMall {
       cart.remove(selectedProduct);
     }
     print('장바구니에서 상품을 $count개 뺐어요!');
+  }
+
+  void restoreLastRemovedItems() {
+    if (lastRemovedItems.isNotEmpty) {
+      lastRemovedItems.forEach((product, count) {
+        cart[product] = (cart[product] ?? 0) + count;
+        totalPrice += product.price * count;
+      });
+      print('삭제한 모든 상품을 장바구니에 복구했습니다!');
+      lastRemovedItems.clear();
+    } else {
+      print('복구할 상품이 없습니다.');
+    }
   }
 
   void showCartSummary() {
@@ -122,18 +153,38 @@ class ShoppingMall {
     cart.forEach((product, count) {
       int itemTotal = product.price * count;
       total += itemTotal;
-      print('${product.name} - ${count}개, ${itemTotal}원');
+      print('${product.name} - $count개, $itemTotal원');
     });
-    print('상품 총 합: ${total}원');
+    print('상품 총 합: $total원');
   }
 
-  void clearCart() {
+  void clearCart({bool askRestore = false}) {
     if (cart.isEmpty || totalPrice == 0) {
       print('이미 장바구니가 비어있습니다.');
+      lastRemovedItems.clear();
     } else {
-      cart.clear();
-      totalPrice = 0;
-      print('장바구니를 초기화합니다.');
+      if (askRestore && lastRemovedItems.isNotEmpty) {
+        print('장바구니에서 삭제한 아이템을 다시 장바구니에 넣으시겠어요?');
+        print('1. yes   2. no');
+        stdout.write('> ');
+        String? input = stdin.readLineSync();
+        if (input == '1') {
+          restoreLastRemovedItems();
+          return;
+        } else if (input == '2') {
+          cart.clear();
+          totalPrice = 0;
+          lastRemovedItems.clear();
+          print('장바구니를 초기화합니다.');
+        } else {
+          print('잘못된 입력입니다. 장바구니를 초기화하지 않습니다.');
+        }
+      } else {
+        cart.clear();
+        totalPrice = 0;
+        lastRemovedItems.clear();
+        print('장바구니를 초기화합니다.');
+      }
     }
   }
 }
@@ -151,11 +202,12 @@ void main() {
 
   bool running = true;
   bool awaitingExitConfirmation = false;
+  bool lastActionWasRemove = false; // 6번 이후 7번 눌렀는지 추적
 
   while (running) {
     if (!awaitingExitConfirmation) {
       print(
-        '\n[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료 / [6] 장바구니에서 상품 빼기 / [7] 장바구니 완전 초기화',
+        '\n[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료 / [6] 장바구니에서 상품 빼기 / [7] 장바구니 초기화',
       );
       stdout.write('> ');
       String? input = stdin.readLineSync();
@@ -163,12 +215,15 @@ void main() {
       switch (input) {
         case '1':
           mall.showProducts();
+          lastActionWasRemove = false;
           break;
         case '2':
           mall.addToCart();
+          lastActionWasRemove = false;
           break;
         case '3':
           mall.showCartSummary();
+          lastActionWasRemove = false;
           break;
         case '4':
           print('정말 종료하시겠습니까?');
@@ -177,12 +232,15 @@ void main() {
           break;
         case '6':
           mall.removeFromCart();
+          lastActionWasRemove = true;
           break;
         case '7':
-          mall.clearCart();
+          mall.clearCart(askRestore: lastActionWasRemove);
+          lastActionWasRemove = false;
           break;
         default:
           print('지원하지 않는 기능입니다! 다시 시도해 주세요');
+          lastActionWasRemove = false;
       }
     } else {
       stdout.write('> ');
